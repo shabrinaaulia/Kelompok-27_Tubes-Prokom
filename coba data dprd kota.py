caf6535407965ae5338166fa6149ef815dc9363d
zabrinasalsabila73@gmail.com <zabrinasalsabila73@gmail.com>
2023-06-08 15:19:50 +0700

    pemilihan dprd kota

--- coba data dprd kota.py
+++ coba data dprd kota.py
@@ -1,25 +1,25 @@
-import csv
+import json
 
-# Fungsi untuk membaca data calon DPRD Kota dari file
+# Fungsi untuk membaca data calon DPRD Kota dari file JSON
 def baca_data_calon():
-    with open("data_calon.csv", "r") as file:
-        data_calon = csv.load(file)
-    return data_calon
+    with open("data_calon_dprd_kota.json", "r") as file:
+        data_calon_dprd_kota = json.load(file)
+    return data_calon_dprd_kota
 
-# Fungsi untuk menampilkan daftar calon DPRD Kota berdasarkan kota
-def tampilkan_daftar_calon(kota, data_calon):
-    print(f"Daftar Calon DPRD Kota {kota}:")
+# Fungsi untuk menampilkan daftar calon DPRD Kota berdasarkan Kota
+def tampilkan_daftar_calon(Kota, data_calon):
+    print(f"Daftar Calon DPRD Kota {Kota}:")
     print("--------------------")
 
-    for calon in data_calon[kota]:
+    for calon in data_calon[Kota]:
         print(f"Nomor Urut: {calon['nomor_urut']}")
         print(f"Nama Calon: {calon['nama']}")
         print("--------------------")
 
-# Fungsi untuk mendapatkan input kota dari pengguna
-def input_kota():
-    kota = input("Masukkan nama kota Anda: ")
-    return kota
+# Fungsi untuk mendapatkan input Kota dari pengguna
+def input_Kota():
+    Kota = input("Masukkan nama Kota Anda: ")
+    return Kota
 
 # Fungsi untuk mendapatkan input nomor urut calon dari pengguna
 def input_nomor_urut():
@@ -27,12 +27,12 @@ def input_nomor_urut():
     return nomor_urut
 
 # Fungsi untuk mendapatkan visi dan misi calon berdasarkan nomor urut
-def get_visi_misi(nomor_urut, data_calon):
+def get_visi_misi(nomor_urut, data_calon_dprd_kota):
     visi = ""
     misi = ""
 
-    for kota in data_calon:
-        for calon in data_calon[kota]:
+    for kota in data_calon_dprd_kota:
+        for calon in data_calon_dprd_kota[kota]:
             if calon['nomor_urut'] == nomor_urut:
                 visi = calon['visi']
                 misi = calon['misi']
@@ -46,15 +46,15 @@ def pemilihan_dprd_kota():
         print("Pemilihan DPRD Kota")
         print("--------------------")
 
-        kota = input_kota()
-        data_calon = baca_data_calon()
+        Kota = input_Kota()
+        data_calon_dprd_kota = baca_data_calon()
 
-        if kota in data_calon:
-            tampilkan_daftar_calon(kota, data_calon)
+        if Kota in data_calon_dprd_kota:
+            tampilkan_daftar_calon(Kota, data_calon_dprd_kota)
 
             nomor_urut = input_nomor_urut()
 
-            visi, misi = get_visi_misi(nomor_urut, data_calon)
+            visi, misi = get_visi_misi(nomor_urut, data_calon_dprd_kota)
 
             print(f"Visi dan Misi Calon:")
             print("--------------------")
@@ -63,12 +63,13 @@ def pemilihan_dprd_kota():
             print("--------------------")
 
             yakin = input("Apakah Anda yakin dengan pilihan Anda? (Ya/Tidak): ")
-            if yakin.lower() != 'y':
+            if yakin.lower() != 'ya':
                 continue
 
-            print("Terima kasih atas partisipasi Anda dalam pemilihan DPRD Kota.")
+            print("Terima kasih atas partisipasi Anda dalam pemilihan DPRD Provinsi.")
             break
         else:
-            print("Kota yang Anda masukkan tidak valid. Silakan coba lagi.")
+            print("Provinsi yang Anda masukkan tidak valid. Silakan coba lagi.")
 
+# Menjalankan program pemilihan DPRD Kota
 pemilihan_dprd_kota()
