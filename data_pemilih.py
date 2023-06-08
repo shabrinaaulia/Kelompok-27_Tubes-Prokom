2e1391cdde10c3d49e13d84594799eb1739124c1
shabrina <shabrinaaulia@student.uns.ac.id>
2023-06-08 15:27:42 +0700

    data pemilih

--- data_pemilih.py
+++ data_pemilih.py
@@ -20,22 +20,5 @@ def input_data_pemilih():
         print("Data pemilih berhasil disimpan.")
     else:
         print("NIM tersebut telah digunakan sebelumnya.")
-
-def tampilkan_data_calon(kota):
-    with open('data_calon_kota.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            if row['kota'] == kota:
-                print("Nama Calon: ", row['nama'])
-                print("Partai: ", row['partai'])
-                print("Kota: ", row['kota'])
-                print("------------------------")
-
-def main():
-    input_data_pemilih()
-
-    kota_pemilih = input("Masukkan kota pemilih: ")
-    tampilkan_data_calon(kota_pemilih)
-
-if __name__ == "__main__":
-    main()
+        
+input_data_pemilih()
